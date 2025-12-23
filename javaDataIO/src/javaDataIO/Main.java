62772e12db03fb65f404bc78bf87a412ab1809b0
elson2121 <elsonsd21@gmail.com>
2025-12-23 10:25:01 +0300

    add the inpute fiel

--- javaDataIO/src/javaDataIO/Main.java
+++ javaDataIO/src/javaDataIO/Main.java
@@ -6,7 +6,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		DataOutputStream dos= new DatOutPutStream(new FileOutPutStream("output"))
+				dos.writeInt(10);
+				dos.writeDouble(100.5);
+				dos.writeBoolean(false);
+				dos.writeUTF("Hello world");
+				System .out.println("your file saved carfully");
+				dos.close();
+				DataInputStream dis=new DataInputStream(new FileInputStream("outputlap.bin"))
+				int valueOfX =dis.readInt();
+				System.out.println(valueOfX);
+				dis.close();
 		
 		
 
