6d897d2e9f33294b53158cb9dcd0aeca4be4bb4e
elson2121 <elsonsd21@gmail.com>
2025-12-23 10:24:10 +0300

    add the inpute fiel

--- javaDataIO/src/javaDataIO/Main.java
+++ javaDataIO/src/javaDataIO/Main.java
@@ -1,9 +1,14 @@
 package javaDataIO;
-
+import java.io. FileInputStream;
+import java.io.FileOutputStream;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		
 
 	}
 
