98aee4ea8ab016e0066a1358545f8aec343eaa99
elson2121 <elsonsd21@gmail.com>
2025-12-23 10:32:30 +0300

    save the file to out put the int values

--- javaDataIO/src/javaDataIO/Main.java
+++ javaDataIO/src/javaDataIO/Main.java
@@ -1,23 +1,32 @@
 package javaDataIO;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io. FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		DataOutputStream dos= new DatOutPutStream(new FileOutPutStream("output"))
+
+				DataOutputStream dos = new DataOutputStream(new FileOutputStream("outputlap.bin"));
 				dos.writeInt(10);
 				dos.writeDouble(100.5);
 				dos.writeBoolean(false);
 				dos.writeUTF("Hello world");
-				System .out.println("your file saved carfully");
+				System.out.println("your file saved carfully");
 				dos.close();
-				DataInputStream dis=new DataInputStream(new FileInputStream("outputlap.bin"))
-				int valueOfX =dis.readInt();
+				
+				DataInputStream dis = new DataInputStream(new FileInputStream("outputlap.bin"));
+				int valueOfX = dis.readInt();
 				System.out.println(valueOfX);
 				dis.close();
-		
+				
+				
+				
+				
+				
 		
 
 	}
