985bccc11e63a1aa29fa3123469343334fa62923
elson2121 <elsonsd21@gmail.com>
2025-12-24 13:10:42 +0300

    set the client

--- javasocketLab/src/javasocketLab/Client.java
+++ javasocketLab/src/javasocketLab/Client.java
@@ -1,10 +1,40 @@
 package javasocketLab;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try {
+            // REPLACE THIS WITH SERVER'S ACTUAL IP ADDRESS
+            String serverIP = "192.168.1.100"; // Change this!
+            Socket s = new Socket(serverIP, 5000);
+            
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeUTF("Hello Server from Client!");
+            
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            System.out.println("Server response: " + dis.readUTF());
+            
+            // Close resources
+            dis.close();
+            dos.close();
+            s.close();
+            
+        } catch (IOException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+            System.out.println("Make sure:");
+            System.out.println("1. Server is running");
+            System.out.println("2. Correct IP address");
+            System.out.println("3. Firewall allows port 5000");
+        }	
+		
+		
 	}
 
 }
